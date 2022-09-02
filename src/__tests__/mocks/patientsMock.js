39ff9fa3bbcd5d033d6ad1875085376c255fd9ae
Vitor Correa <vitorcorrea18@gmail.com>
2022-09-02 16:19:43 -0300

    chore: create mock

---
+++ src/__tests__/mocks/patientsMock.js
@@ -0,0 +1,83 @@
+// em aberto até setembro
+
+export default [
+  {
+    name: 'Fulana da Silva',
+    treatment: 'Aparelho ortodôntico',
+    total: '954.00',
+    startDate: '26-02-2022',
+    installments: 12,
+  },
+
+  {
+    name: 'Silvério Silveira',
+    treatment: 'Restauração de dente',
+    total: '630.00',
+    startDate: '13-04-2022',
+    installments: 6,
+  },
+
+  {
+    name: 'Dino Sauro',
+    treatment: 'Branqueamento dentário',
+    total: '954.00',
+    startDate: '05-04-2022',
+    installments: 12,
+  },
+
+  {
+    name: 'Ivete Sangalo',
+    treatment: 'Limpesa',
+    total: '130.00',
+    startDate: '01-09-2022',
+    installments: 1,
+  },
+
+  {
+    name: 'Fernanda Montenegro',
+    treatment: 'Limpesa',
+    total: '130.00',
+    startDate: '02-09-2022',
+    installments: 1,
+  },
+
+  {
+    name: 'Almir Sater',
+    treatment: 'Branqueamento dentário',
+    total: '954.00',
+    startDate: '20-02-2022',
+    installments: 10,
+  },
+
+  {
+    name: 'Maria Betânia',
+    treatment: 'Prótese Dentária',
+    total: '720.00',
+    startDate: '26-02-2022',
+    installments: 8,
+  },
+
+  {
+    name: 'Cicrana Golçalves',
+    treatment: 'Aparelho ortodôntico',
+    total: '954.00',
+    startDate: '13-01-2022',
+    installments: 12,
+  },
+
+  {
+    name: 'Beltrano Sousa',
+    treatment: 'Limpesa',
+    total: '954.00',
+    startDate: '19-12-2021',
+    installments: 12,
+  },
+
+  {
+    name: 'Siqueira Junior',
+    treatment: 'Branqueamento dentário',
+    total: '830.00',
+    startDate: '20-10-2021',
+    installments: 12,
+  },
+];
