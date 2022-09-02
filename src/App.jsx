5966ca2f66676df5658a02148b2a70c57529e7ff
Vitor Correa <vitorcorrea18@gmail.com>
2022-09-02 12:53:02 -0300

    feat: create main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/header';
+import months from './utils/months';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 
@@ -7,6 +8,47 @@ function App() {
   return (
     <>
       <Header />
+      <section className="main-section">
+        <div className="period-div">
+          <label
+            className="form-label"
+            htmlFor="month-select"
+          >
+            Mês
+            <select className="form-select" id="month-select">
+              {
+                months.map((m) => (<option>{m}</option>))
+              }
+            </select>
+          </label>
+          <label
+            className="form-label"
+            htmlFor="year-select"
+          >
+            Ano
+            <select className="form-select" id="year-select">
+              <option>2022</option>
+              <option>2021</option>
+            </select>
+          </label>
+        </div>
+        <div className="value-div">
+          <span className="span-total">
+            Total recebido:
+          </span>
+          <span className="span-total">
+            R$ xxx,xx
+          </span>
+        </div>
+        <div className="value-div">
+          <span className="span-total">
+            Total esperado:
+          </span>
+          <span className="span-total">
+            R$ xxx,xx
+          </span>
+        </div>
+      </section>
       <h1>Hello World</h1>
     </>
 
