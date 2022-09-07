45af10fd2cccb24bf1ff010ca1028bcda6acd320
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-07 14:14:10 -0300

    fix: endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,10 @@ const api = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
-const fetchPatientsApi = (month) => {
-  const endpoint = 'patients';
-  const { data } = api.get(endpoint, { month });
+const fetchPatientsApi = async () => {
+  const endpoint = '/patients';
+  const { data } = await api.get(endpoint);
+
   return data;
 };
 
