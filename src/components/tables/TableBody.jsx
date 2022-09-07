730327ed64b22cb8446996cf4c356c7cee47e647
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-07 14:13:02 -0300

    feat: create component for table body

---
+++ src/components/tables/TableBody.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { formatDate } from '../../helpers';
+
+export default function TableBody({ data }) {
+  return (
+    <tbody className="table-body">
+      {
+        data.map(({
+          id, name, treatment, installmentsValue, installment, paymentIndex,
+        }, index) => (
+          <tr key={id}>
+            <td data-testid={`table-name-${index}`}>
+              {name}
+            </td>
+
+            <td data-testid={`table-treatment-${index}`}>
+              {treatment.name}
+            </td>
+
+            <td data-testid={`table-installments-${index}`}>
+              {`${paymentIndex} ${formatDate(installment.date)}`}
+            </td>
+
+            <td data-testid={`table-installment-value-${index}`}>
+              {`R$ ${installmentsValue}`}
+            </td>
+
+            <td data-testid={`table-total-${index}`}>
+              {`R$ ${treatment.price}`}
+            </td>
+
+            <td data-testid={`table-status-${index}`}>
+              {installment.status}
+            </td>
+
+            <td data-testid={`table-btns-${index}`}>
+              { /* implementar os botões que marcam o pagamento */}
+              botões
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+  );
+}
+
+TableBody.propTypes = {
+  data: PropTypes.instanceOf(Array).isRequired,
+};
--- src/components/tables/index.jsx
+++ src/components/tables/index.jsx
@@ -1,26 +1,12 @@
 import React, { useContext } from 'react';
 import Context from '../../context/context';
+import TableBody from './TableBody';
 
 export default function Table() {
   // Table que vai exibir os detalhes dos pacientes
 
   const { patients } = useContext(Context);
-
-  const data = patients.map((patient) => {
-    // Retira o array contendo todas as parcelas
-    const { installments: allMonthsI, ...patientWithoutI } = patient;
-
-    const [crrMonthI] = allMonthsI
-      .filter((installment) => (installment.date.split('-')[1] === '09')); // filtra para encontrar a parcela do mês atual
-
-    // Retorno um objeto com os dados do paciente contendo
-    // parcela atual e o index do pagamento que indica quantas parcelas ainda restam.
-    return {
-      ...patientWithoutI,
-      installment: crrMonthI,
-      paymentIndex: `${allMonthsI.indexOf(crrMonthI) + 1}/${allMonthsI.length}`,
-    };
-  });
+  const searchingMessage = 'Buscando Paciente';
 
   return (
     <table className="patient-table">
@@ -36,48 +22,13 @@ export default function Table() {
           <th>Marcar como Pago</th>
         </tr>
       </thead>
-      <tbody className="table-body">
-        {
-          data.map(({
-            id, name, treatment, total, installment, paymentIndex,
-          }, index) => {
-            const monthValue = (total / patients[index].installments.length).toFixed(2);
-
-            return (
-              <tr key={id}>
-                <td data-testid={`table-name-${index}`}>
-                  {name}
-                </td>
-
-                <td data-testid={`table-treatment-${index}`}>
-                  {treatment}
-                </td>
-
-                <td data-testid={`table-installments-${index}`}>
-                  {`${paymentIndex} ${installment.date}`}
-                </td>
-
-                <td data-testid={`table-installment-value-${index}`}>
-                  {monthValue}
-                </td>
-
-                <td data-testid={`table-total-${index}`}>
-                  {total}
-                </td>
-
-                <td data-testid={`table-status-${index}`}>
-                  {installment.status}
-                </td>
-
-                <td data-testid={`table-btns-${index}`}>
-                  { /* implementar os botões que marcam o pagamento */}
-                  botões
-                </td>
-              </tr>
-            );
-          })
-        }
-      </tbody>
+      {
+        patients.length
+          ? (<TableBody data={patients} />)
+          : (
+            <tbody><tr><th>{searchingMessage}</th></tr></tbody>
+          )
+      }
     </table>
   );
 }
