bc9c2dda7bc5e3af8937d44b6a47bf47a95ae3ca
Vitor J.V. Correa <90797919+VitorCorrea18@users.noreply.github.com>
2022-09-12 20:39:24 -0300

    update API URL to use with localhost

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://softeo-api.herokuapp.com',
+  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+});
 });
 
 export const fetchPatientsApi = async () => {
