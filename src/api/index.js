4f3b97cc8e168a719103d824a84b470a5edbaa0e
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-12 10:37:09 -0300

    fix: removing port at api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://softeo-api.herokuapp.com/:${process.env.API_PORT || '3001'}`,
+  baseURL: 'https://softeo-api.herokuapp.com',
 });
 
 export const fetchPatientsApi = async () => {
